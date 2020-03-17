8dcb6c357ceeadf7c8b652a112c1d1f5fa9d6ca6
Alexander Melo <alexandermelox@gmail.com>
2020-03-17 15:53:15 -0500

    added the ability to search for allCases and a specific case

--- index.js
+++ index.js
@@ -1,26 +1,119 @@
 const { ApolloServer, gql } = require('apollo-server');
+const GraphQLLong = require('graphql-type-long');
+const axios = require('axios');
+const moment = require('moment');
 
-const apiURL = 'https://covid19.mathdro.id/api';
+const urls = {
+  base: 'https://covid19.mathdro.id/api',
+  cases: 'https://covid19.mathdro.id/api/confirmed',
+};
+
+function getAllCases() {
+  return axios.get(urls.cases);
+}
+
+function getBaseData() {
+  return axios.get(urls.base);
+}
 
 async function start() {
   const typeDefs = gql`
-    type Confirmed {
-      hello: String!
+    scalar Long
+
+    enum DateFormats {
+      FULL_DATE
+      SINCE_LAST_UPDATED
+      DEFAULT
+    }
+
+    type Meta {
+      image: String!
+      githubRepo: String!
+      lastUpdate: String!
+    }
+
+    type Stats {
+      confirmed: Int!
+      recovered: Int!
+      deaths: Int!
+    }
+
+    type Case {
+      provinceState: String
+      countryRegion: String
+      lastUpdate: String
+      lat: Float
+      long: Float
+      confirmed: Int
+      recovered: Int
+      deaths: Int
+      iso2: String
+      iso3: String
     }
 
     type Query {
-      allConfirmed: [Confirmed!]!
+      allCases: [Case!]!
+      case(countryRegion: String!): Case!
+      stats: Stats!
+      meta(format: DateFormats): Meta!
     }
   `;
 
   const resolvers = {
     Query: {
-      hello() {
-        return {
-          hello: 'Hello, world!',
+      async allCases(_, {}) {
+        const allCases = await getAllCases();
+        const now = moment();
+        allCases.data.forEach(c => {
+          c.lastUpdate = now.from(moment(c.lastUpdate));
+        });
+        return allCases.data;
+      },
+      async case(_, { countryRegion }) {
+        const allCases = await getAllCases();
+
+        const foundCaseByRegion = allCases.data.find(
+          c => c.countryRegion === countryRegion
+        );
+
+        return foundCaseByRegion;
+      },
+      async stats() {
+        const { data } = await getBaseData();
+        const stats = {
+          confirmed: data.confirmed.value,
+          recovered: data.recovered.value,
+          deaths: data.deaths.value,
         };
+        return stats;
+      },
+      async meta(_, { format }) {
+        const { data } = await getBaseData();
+        const now = moment();
+        let lastUpdate;
+
+        switch (format) {
+          case 'FULL_DATE':
+            lastUpdate = moment(data.lastUpdate).format(
+              'dddd, MMMM Do YYYY, h:mm:ss a'
+            );
+            break;
+          case 'SINCE_LAST_UPDATED':
+            lastUpdate = now.from(moment(data.lastUpdate));
+            break;
+          default:
+            lastUpdate = data.lastUpdate;
+        }
+
+        const meta = {
+          image: data.image,
+          githubRepo: data.source,
+          lastUpdate,
+        };
+        return meta;
       },
     },
+    Long: GraphQLLong,
   };
 
   const server = new ApolloServer({ typeDefs, resolvers });
